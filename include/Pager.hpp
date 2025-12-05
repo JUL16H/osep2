0877f1b6b1ba1922e45a2471a95df5971c535cd6
ldx <17301339186@163.com>
2025-12-05 13:00:02 +0800

    .

--- include/Pager.hpp
+++ include/Pager.hpp
@@ -1,25 +1,36 @@
 #pragma once
-#include <algorithm>
 #include <vector>
 #include <iostream>
 #include <format>
+#include <unordered_map>
+#include <algorithm>
 
 template <int N>
 class PagerBase {
 public:
     PagerBase(std::vector<unsigned> _reqs) : reqs(_reqs) {};
     void run() {
+        std::cout << "\033[1;32m" << std::string(30, '=') << name()
+         << std::string(30, '=') << "\033[0m\n";
         while (idx < reqs.size()) {
             auto i = insert();
             show(i);
         }
     }
+    void operator()() { run(); }
 protected:
     unsigned insert() {
         auto p = reqs[idx++];
         if (!p)
             return 0;
-        std::cout << "req:" << p << "\n";
+        for (unsigned i = 0; i < reqs.size(); i++) {
+            if (i == idx - 1)
+                std::cout << "\033[1;32m";
+            std::cout << reqs[i] << " ";
+            if (i == idx - 1)
+                std::cout << "\033[0m";
+        }
+        std::cout << '\n';
         for (unsigned i = 0; i < N; i++)
             if (pages[i] == p)
                 return i;
@@ -29,7 +40,9 @@ protected:
                 return i;
             }
         }
-        return replace(p);
+        auto i = replace(p);
+        pages[i] = p;
+        return i;
     }
 
     virtual unsigned replace(unsigned p) = 0;
@@ -51,14 +64,42 @@ protected:
         std::cout << "\n+";
         for (unsigned i = 0; i < N; i++)
             std::cout << "--+";
-        std::cout << "\n";
+        std::cout << "\n\n";
     }
 
 protected:
     unsigned idx = 0;
     std::array<unsigned, N> pages = { 0 };
     std::vector<unsigned> reqs;
+    virtual const char* name() const noexcept = 0;
 };
+
+template <unsigned N>
+class Pager_OPT : public PagerBase<N> {
+public:
+    Pager_OPT(std::vector<unsigned> _reqs) : PagerBase<N>(_reqs) {}
+
+    unsigned replace(unsigned p) override {
+        std::unordered_map<unsigned, unsigned> mp;
+        for (unsigned i = 0; i < N; i++)
+            mp[this->pages[i]] = i;
+
+        for (unsigned i = this->idx; i < this->reqs.size(); i++) {
+            if (mp.size() == 1)
+                break;
+            if (mp.count(this->reqs[i]))
+                mp.erase(this->reqs[i]);
+        }
+
+        return mp.begin()->second;
+    }
+
+protected:
+    const char* name() const noexcept override {
+        return "OPT";
+    }
+};
+
 
 template <unsigned N>
 class Pager_FIFO : public PagerBase<N> {
@@ -66,10 +107,14 @@ public:
     Pager_FIFO(std::vector<unsigned> _reqs) : PagerBase<N>(_reqs) {}
 
     unsigned replace(unsigned p) override {
-        this->pages[cur] = p;
         unsigned rst = cur;
         cur = (cur + 1) % N;
         return rst;
+    }
+
+protected:
+    const char* name() const noexcept override {
+        return "FIFO";
     }
 
 private:
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@ int main() {
     PageReqGenerator req_generator;
     req_generator.randomize(30);
     auto vec = req_generator.get_page_req();
-    std::ranges::for_each(vec, [](const auto& item){std::cout << item << " "; });
-    std::cout << '\n';
+    // std::ranges::for_each(vec, [](const auto& item){std::cout << item << " "; });
+    // std::cout << '\n';
 
-    Pager_FIFO<5> page(req_generator.get_page_req());
-    page.run();
+    Pager_OPT<5>{req_generator.get_page_req()}();
+    Pager_FIFO<5>{req_generator.get_page_req()}();
 
     return 0;
 }
